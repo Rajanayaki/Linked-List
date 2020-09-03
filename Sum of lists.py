10e646035b0bbe4d75ab1f3978f207908da464bf
Rajanayaki S <45528670+Rajanayaki@users.noreply.github.com>
2020-09-03 09:44:29 +0530

    Correct program

--- Sum of lists.py
+++ Sum of lists.py
@@ -1,26 +1,23 @@
-class node:
-  def  __init__(self,data):
+cclass node:
+  def __init__(self,data):
     self.data=data
     self.next=None
 
 class linkedlist:
-  
   def __init__(self):
     self.head=None
-  
-  def push(self,data):
-    newnode=node(data)
-    if self.head is None:
-      self.head=newnode
-      newnode.next=None
-    else:
-      newnode.next=self.head
-      self.head=newnode
 
-  def sumoflists(self,head1,head2,head3):
-    curr1,curr2=head1,head2
+  def add(self,data):
+    newnode=node(data)
+    newnode.next = self.head 
+    self.head = newnode
+
+
+  def sumlists(self,head1,head2):
+    curr1=head1
+    curr2=head2
+    
     carry=0
-    print(head1.data,)
     while curr1!=None and curr2!=None:
       sum=curr1.data+curr2.data+carry
       if sum>=10:
@@ -28,36 +25,50 @@ class linkedlist:
         sum=sum%10
       else:
         carry=0
-        sum=sum
       temp=node(sum)
-      if head3 is None:
-        head3=temp
-       else:
+      if self.head is None:
+        self.head=temp
+      else:
         prev.next=temp
       prev=temp
       curr1=curr1.next
       curr2=curr2.next
-    if carry!=0:
+    if carry>0:
       temp.next=node(carry)
-    return head3
+    return self.head
   
+  def reverse(self):
+    curr=self.head
+    prev=None
+    while curr is not None:
+      next=curr.next
+      curr.next=prev
+      prev=curr
+      curr=next
+    self.head=prev
+
   def printlist(self):
-    temp=self.head
-    while(temp.next.next!=None):
-      print(temp.data,end=" ")
-      temp=temp.next
-    print(temp.data,temp.next.data)
-    
+    while (self.head.next!=None):
+      print(self.head.data,end=" ")
+      self.head=self.head.next
+    print(self.head.data)
+
+        
 ll1=linkedlist()
+size1=int(input())
+datalist1=list(map(int,input().split()))
+for i in range(size1):
+  ll1.add(datalist1[i])
 ll2=linkedlist()
-elements1=list(map(int,input().split()))
-for i in range(len(elements1)):
-  ll1.push(elements1[i])
-elements2=list(map(int,input().split()))
-for i in range(len(elements2)):
-  ll2.push(elements2[i])
-ll1.printlist()
-ll2.printlist()
-ll3=linkedlist()
-ll3.head=ll3.sumoflists(ll1.head,ll2.head,ll3.head)
-ll3.printlist()
+size2=int(input())
+datalist2=list(map(int,input().split()))
+for i in range(size2):
+  ll2.add(datalist2[i])
+ll=linkedlist()
+ll.head=ll.sumlists(ll1.head,ll2.head)
+ll.reverse()
+ll.printlist()
+
+
+
+
