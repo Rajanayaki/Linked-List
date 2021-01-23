23db0f1db5683dd65cd22f8758f4a7103a17a6d1
Rajanayaki S <45528670+Rajanayaki@users.noreply.github.com>
2021-01-23 11:43:58 +0530

    Create Merge point of two ll.py

---
+++ Merge point of two ll.py
@@ -0,0 +1,65 @@
+#!/bin/python3
+#hackerrank solution
+
+import math
+import os
+import random
+import re
+import sys
+
+class SinglyLinkedListNode:
+    def __init__(self, node_data):
+        self.data = node_data
+        self.next = None
+
+class SinglyLinkedList:
+    def __init__(self):
+        self.head = None
+        self.tail = None
+
+    def insert_node(self, node_data):
+        node = SinglyLinkedListNode(node_data)
+
+        if not self.head:
+            self.head = node
+        else:
+            self.tail.next = node
+
+
+        self.tail = node
+
+def print_singly_linked_list(node, sep, fptr):
+    while node:
+        fptr.write(str(node.data))
+
+        node = node.next
+
+        if node:
+            fptr.write(sep)
+
+# Complete the findMergeNode function below.
+
+#
+# For your reference:
+#
+# SinglyLinkedListNode:
+#     int data
+#     SinglyLinkedListNode next
+#
+#
+def findMergeNode(head1, head2):
+    curr1=head1
+    curr2=head2
+    while curr1!=curr2:
+        if curr1 is None:
+            curr1=head1
+        else:
+            curr1=curr1.next
+        if curr2 is None:
+            curr2=head2
+        else:
+            curr2=curr2.next
+    return curr2.data
+            
+
+if __name__ == '__main__':
